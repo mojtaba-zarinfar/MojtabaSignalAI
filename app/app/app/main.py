da74b8dbdca3d4835a632dbb00c8dbfa12d47162
mojtaba-zarinfar <89013714+mojtaba-zarinfar@users.noreply.github.com>
2026-07-05 11:59:14 +0330

    Update main.py

--- app/app/app/main.py
+++ app/app/app/main.py
@@ -1,46 +1,86 @@
 import time
 import pandas as pd
 
+from app.config import SYMBOLS, TIMEFRAMES, CHECK_INTERVAL
 from app.market import get_ohlcv
 from app.indicators import add_indicators
-from app.strategy import get_signal
+from app.strategy import get_final_signal, validate_signal
 from app.telegram_bot import send_message, format_signal
-from app.config import SYMBOLS, TIMEFRAMES, CHECK_INTERVAL
+from app.btc_filter import analyze_btc
 
+
+# ==========================================
+# Get BTC Context
+# ==========================================
+
+def get_btc_context():
+
+    data = get_ohlcv("BTC/USDT", timeframe="5m", limit=200)
+
+    df = pd.DataFrame(data, columns=[
+        "timestamp", "open", "high", "low", "close", "volume"
+    ])
+
+    df = add_indicators(df)
+
+    return analyze_btc(df)
+
+
+# ==========================================
+# Get Market Data
+# ==========================================
+
+def get_symbol_data(symbol, timeframe):
+
+    data = get_ohlcv(symbol, timeframe=timeframe, limit=200)
+
+    df = pd.DataFrame(data, columns=[
+        "timestamp", "open", "high", "low", "close", "volume"
+    ])
+
+    df = add_indicators(df)
+
+    return df
+
+# ==========================================
+# Run Analysis
+# ==========================================
 
 def run_analysis():
+
+    btc_result = get_btc_context()
+
     for symbol in SYMBOLS:
 
         try:
-            # گرفتن دیتا
-            data = get_ohlcv(symbol, timeframe="5m", limit=200)
 
-            df = pd.DataFrame(data, columns=[
-                "timestamp", "open", "high", "low", "close", "volume"
-            ])
+            for tf in TIMEFRAMES:
 
-            # اندیکاتورها
-            df = add_indicators(df)
+                df = get_symbol_data(symbol, tf)
 
-            latest = df.iloc[-1]
-            current_price = latest["close"]
-            entry_price = df["close"].mean()  # ساده برای نسخه 1
+                signal = get_final_signal(df, btc_result)
 
-            # سیگنال
-            signal = get_signal(latest, entry_price, current_price)
-
-            if signal:
+                if not validate_signal(signal):
+                    continue
 
                 text = format_signal(signal, symbol)
+
                 send_message(text)
 
         except Exception as e:
             print(f"Error in {symbol}: {e}")
 
 
+# ==========================================
+# Main Loop
+# ==========================================
+
 if __name__ == "__main__":
-    print("Bot started...")
+
+    print("Bot Started...")
 
     while True:
+
         run_analysis()
+
         time.sleep(CHECK_INTERVAL)
